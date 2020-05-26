499b03fb51df63eac8b1cccceb6feaae048c7f01
Scott-David Jones <scott.jones@autumndev.co.uk>
2020-05-26 08:43:02 +0100

    Casting variable as a float
    Signed-off-by: Scott-David Jones <scott.jones@autumndev.co.uk>

--- config/blackbelt.php
+++ config/blackbelt.php
@@ -5,5 +5,5 @@ declare(strict_types=1);
 return [
     'client_id'         => env('BLACKBELT_CLIENT_ID', ''),
     'client_secret'     => env('BLACKBELT_CLIENT_SECRET', ''),
-    'timeout'           => env('BLACKBELT_CLIENT_TIMEOUT', 5.0),
+    'timeout'           => (float) env('BLACKBELT_CLIENT_TIMEOUT', 5.0),
 ];
